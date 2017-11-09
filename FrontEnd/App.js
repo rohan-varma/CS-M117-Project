8e2b7d3e986d5d0f011ab51e39fce2b0413a4b33
Rohan Varma <rvarm1@gmail.com>
2017-11-08 18:00:07 -0800

    figuring out how to add components

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Navigator, AppRegistry } from 'react-native';
+import Movies from './src/Movies'
+
+
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text> Hello {this.props.name}! </Text>
+      );
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the best CS M117 project in CS M117 history!</Text>
+        <Text>This is the best CS M117 project ever!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Greeting name='rohan' />
       </View>
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -21,3 +35,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// import React, { Component } from 'react';
+// import { AppRegistry, Text, View } from 'react-native';
+
+// class Greeting extends Component {
+//   render() {
+//     return (
+//       <Text>Hello {this.props.name}!</Text>
+//     );
+//   }
+// }
+
+// export default class LotsOfGreetings extends Component {
+//   render() {
+//     return (
+//       <View style={{alignItems: 'center'}}>
+//         <Greeting name='Rexxar' />
+//         <Greeting name='Jaina' />
+//         <Greeting name='Valeera' />
+//       </View>
+//     );
+//   }
+// }
