7fb4716283cb6da492c8e69cda3f698a913786aa
prathyushk <prathyush.katukojwala@gmail.com>
2017-11-11 11:29:46 -0800

    [BackEnd][models] Add alliance schema

---
+++ BackEnd/models/alliance.js
@@ -0,0 +1,8 @@
+import mongoose, { Schema } from 'mongoose';
+
+var allianceSchema = new Schema({
+    allied = [{ type: Schema.Types.ObjectId, ref: 'player' }],
+    targets = [{ type: Schema.Types.ObjectId, ref: 'player' }],
+});
+
+export default mongoose.model('alliance', allianceSchema);
