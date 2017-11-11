8c28e4b0b29446974dfad30f74e61bcd78e73299
prathyushk <prathyush.katukojwala@gmail.com>
2017-11-11 10:12:04 -0800

    [BackEnd][models] Add game schema

---
+++ BackEnd/models/game.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from 'mongoose';
+
+var gameSchema = new Schema({
+    game-code: {
+	type: String,
+	unique: true,
+    },
+    organizer-name: String,
+    central-safe-zone: { type: Schema.Types.ObjectId, ref: 'safe-zone'},
+    players: [{ type: Schema.Types.ObjectId, ref: 'player'}],
+});
+
+export default mongoose.model('game', gameSchema);
