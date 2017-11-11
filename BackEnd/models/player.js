dc7ed1b5c2c56362e9f67c58f14cf526b6221fb8
prathyushk <prathyush.katukojwala@gmail.com>
2017-11-11 10:37:56 -0800

    [BackEnd][models] Add player schema

---
+++ BackEnd/models/player.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from 'mongoose';
+
+var playerSchema = new Schema({
+    username: {
+	type: String,
+	unique: true,
+    },
+    alive: Boolean,
+    game: [{ type: Schema.Types.ObjectId, ref: 'game' }],
+    my-safe-zone: { type Schema.Types.ObjectId, ref: 'safe-zone' },
+    target: { type Schema.Types.ObjectId, ref: 'player' },
+    location: {
+	type: "Point",
+	coordinates: [x, y]
+    },
+});
+
+export default mongoose.model('player', playerSchema);
