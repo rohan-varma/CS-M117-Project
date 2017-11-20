8adc3afa825ad882023a8c29066d441e68f64aaa
prathyushk <prathyush.katukojwala@gmail.com>
2017-11-19 17:31:34 -0800

    [BackEnd] Add player location APIs

--- BackEnd/routes.js
+++ BackEnd/routes.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import config from './config';
-import DB from './db';
+import mongoose from 'mongoose';
+import Player from './models/player';
 
 const router = express.Router();
+
+var mongoDB = config.client.mongodb.defaultUri + '/' + config.client.mongodb.defaultDatabase;
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 router.get('/', (req, res, next) => {
     var test = {
@@ -14,6 +24,27 @@ router.get('/', (req, res, next) => {
 
 router.get('/config', (req, res, next) => {
     res.json(config.client);
+});
+
+router.post('/updateLocation', (req, res) => {
+    Player.findOneAndUpdate({ username: req.body.username },
+			    { $set: { location: [req.body.x, req.body.y] } },
+			    (err, doc) => {
+				if (err)
+				    res.send(err);
+				else
+				    res.sendStatus(200);
+			    });
+});
+
+router.get('/getTargetLocation', (req, res) => {
+    Player.findOne({ username: req.query.username }, (err, obj) => {
+	if (err)
+	    res.send(err);
+	else {
+	    res.send(obj.location);
+	}
+    });
 });
 
 module.exports = router;
