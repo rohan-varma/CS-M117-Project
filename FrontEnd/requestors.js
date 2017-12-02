0d667a904a4797cdb3d5aaa419b5d97181d1baf0
Rohan Varma <rvarm1@gmail.com>
2017-12-02 05:44:54 -0800

    final changes

--- FrontEnd/requestors.js
+++ FrontEnd/requestors.js
@@ -1,6 +1,11 @@
 
+const createGameURL = 'http://localhost:3000/BluA/createGame';
+const addUserURL = 'http://localhost:3000/BluA/addUser';
+const startGameURL = 'http://localhost:3000/BluA/startGame';
+const updateLocationURL = 'http://localhost:3000/BluA/updateLocation';
+
 //these are the functions that interface with the backend. Import and use them wherever in the fronted its needed.
-const createGame = body => fetch('http://localhost:3000/BluA/createGame', {
+const createGame = body => fetch(createGameURL, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -9,7 +14,7 @@ const createGame = body => fetch('http://localhost:3000/BluA/createGame', {
     body: body,
   }).then(res => res.json())
 
-const addUserToGame = body => fetch('http://localhost:3000/BluA/addUser', {
+const addUserToGame = body => fetch(addUserURL, {
   method: 'POST',
   headers: {
       Accept: 'application/json',
