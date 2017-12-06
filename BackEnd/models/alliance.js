e8043ad7c845bb65a032889b6e016e44daa9bbba
tanya-al <talrehan@ucla.edu>
2017-12-06 05:55:24 -0800

    schema change

--- BackEnd/models/alliance.js
+++ BackEnd/models/alliance.js
@@ -3,6 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 var allianceSchema = new Schema({
 	allies: [{ type: Schema.Types.ObjectId, ref: 'player' }],
 	targets: [{ type: Schema.Types.ObjectId, ref: 'player' }],
+	dictionary: [{target: Schema.Types.ObjectId, ally: Schema.Types.ObjectId}],
 });
 
 export default mongoose.model('alliance', allianceSchema);
