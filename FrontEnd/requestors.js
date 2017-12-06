e9bd0f268259cded3dd07567efac8c5372247fb4
Prathyush Katukojwala <prathyush.katukojwala@gmail.com>
2017-12-06 07:33:08 -0800

    Update requestors.js

--- FrontEnd/requestors.js
+++ FrontEnd/requestors.js
@@ -1,4 +1,4 @@
-const ServerURL = 'http://localhost:3000/BluA'
+const ServerURL = 'http://35.225.245.61/BluA'
 const gameExistsURL = ServerURL + '/gameExists';
 const createGameURL = ServerURL + '/createGame';
 const addUserURL = ServerURL + '/addUser';
